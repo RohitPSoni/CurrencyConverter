5fdaf5912a32f2860f27a9fa36fbb63b2b1bd33b
rohit.soni <rohit.soni@go-jek.com>
2021-07-13 17:46:11 +0530

    Added jacoco

--- app/src/main/java/com/example/currencyexchange/network/Api.kt
+++ app/src/main/java/com/example/currencyexchange/network/Api.kt
@@ -1,17 +1,11 @@
 package com.example.currencyexchange.network
 
 import com.example.currencyexchange.BuildConfig
-import com.example.currencyexchange.network.data.AvailableCurrencyData
 import com.example.currencyexchange.network.data.LiveCurrencyData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
-
-    @GET("list")
-    suspend fun getAllAvailableCurrencies(
-        @Query("access_key") apikey: String = BuildConfig.API_KEY
-    ): AvailableCurrencyData
 
     @GET("live")
     suspend fun getLiveRate(
